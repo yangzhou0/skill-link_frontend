4701d23d349aec1f70792a5e1af71e316de14ed5
Yang Zhou <yzhou3991@gmail.com>
2021-01-07 16:41:04 -0500

    fix styling issue;

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,7 +6,7 @@ import './App.css'
 
 function App() {
   return ( 
-    <div>
+    <div className = 'app'>
       <Router>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/jobs" component={JobPage} />
