33769f4488a0e74137af57cdd1a23fa8aa347222
Yang Zhou <yzhou3991@gmail.com>
2021-01-07 17:34:37 -0500

    create Joboverview component and pass associatedskills to it

--- front-end/src/pages/JobPage.js
+++ front-end/src/pages/JobPage.js
@@ -1,12 +1,18 @@
 import React,{useState, useEffect} from 'react'
+
 import SearchBar from '../components/SearchBar'
+import JobOverview from '../components/JobOverview'
+
 import './css/JobPage.css'
 export default function JobPage() {
-
-
+  const [associatedSkills, setAssociatedSkills] = useState({});
+  const updateAssociatedSkills = (associatedSkills) => {
+    setAssociatedSkills(associatedSkills)
+  }
   return (
     <div className = 'JobPage'>
-      <SearchBar />
+      <SearchBar updateAssociatedSkills = {updateAssociatedSkills}/>
+      <JobOverview associatedSkills = {associatedSkills}/>
     </div>
   )
 }
