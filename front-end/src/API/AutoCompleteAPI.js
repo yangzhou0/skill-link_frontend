7f48d86390b851deecd6b6a23273ee47dbe4a54c
Yang Zhou <yzhou3991@gmail.com>
2021-01-11 11:23:26 -0500

    write autocomplete for skill names

--- front-end/src/API/AutoCompleteAPI.js
+++ front-end/src/API/AutoCompleteAPI.js
@@ -1,10 +1,10 @@
-const BASE_URL = 'http://api.dataatwork.org/v1/jobs/autocomplete?begins_with=' 
 const getJobAutoCompleteResults = async (input) => {
+  const BASE_URL_JOB = 'http://api.dataatwork.org/v1/jobs/autocomplete?begins_with=' 
   let result = []
   if (!input){ // empty input, will return no autocomplete 
     return result
   }
-  const response = await fetch(`${BASE_URL}${input}`);
+  const response = await fetch(`${BASE_URL_JOB}${input}`);
   const data = await response.json();
   if (data.error){
     return result
@@ -23,7 +23,27 @@ const getJobAutoCompleteResults = async (input) => {
 };
 
 const getSkillAutoCompleteResults = async (input) => {
+const BASE_URL_SKILL = 'http://api.dataatwork.org/v1/skills/autocomplete?begins_with=' 
+  let result = []
+  if (!input){ // empty input, will return no autocomplete 
+    return result
+  }
+  const response = await fetch(`${BASE_URL_SKILL}${input}`);
+  const data = await response.json();
+  if (data.error){
+    return result
+  }
+  let dataLen = data.length
 
+  for (let i = 0; i < 10; i++){ // return only first ten responses
+    if (i >= dataLen){ // break if there is less than 10 responses
+      break;
+    }
+    result.push({
+      "skillName":data[i]['suggestion'],
+      "uuid":data[i]['uuid']})
+  }
+  return result;
 }
 export {
   getJobAutoCompleteResults,
