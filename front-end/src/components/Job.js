93788a9a5e1b5dddb371eff3cab7e8c7112f02c3
Yang Zhou <yzhou3991@gmail.com>
2021-01-12 18:11:50 -0500

    add view/hide details button

--- front-end/src/components/Job.js
+++ front-end/src/components/Job.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { Card, Badge, Button, Collapse } from 'react-bootstrap'
+import ReactMarkdown from 'react-markdown'
 
 
 export default function Job({job}) {
+  const [open, setOpen] = useState(false)
+
   return (
     <Card>
       <Card.Body>
@@ -16,8 +19,25 @@ export default function Job({job}) {
               </Card.Subtitle>
               <Badge variant="secondary" className="mr-2">{job.type}</Badge>
               <Badge variant="secondary">{job.location}</Badge>
+              <div style={{ wordBreak: 'break-all' }}>
+                <ReactMarkdown source={job.how_to_apply} />
+              </div>
             </div>
+            <img className="d-none d-md-block" height="50" alt={job.company} src={job.company_logo} />
         </div>
+        <Card.Text>
+          <Button
+            onClick={() => setOpen(!open)}
+            variant="primary"
+          >
+            {open ? 'Hide Details' : 'View Details'}
+          </Button>
+        </Card.Text>
+        <Collapse in={open}>
+          <div className="mt-4">
+            <ReactMarkdown source={job.description} />
+          </div>
+        </Collapse>
       </Card.Body>
     </Card>
   )
