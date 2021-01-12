d759a1527a3166c9bc62b5b95bf19329529a2705
Yang Zhou <yzhou3991@gmail.com>
2021-01-12 17:07:29 -0500

    import react-bootstrap to render job card better

--- front-end/src/pages/JobListingPage.js
+++ front-end/src/pages/JobListingPage.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react';
 import useFetchJobs from '../API/useFetchJobs'
 import { Container } from 'react-bootstrap'
+import Job from '../components/Job'
+
 export default function JobListingPage() {
   const { jobs, loading, error } = useFetchJobs()
+  const [params, setParams] = useState({})
+  const [page, setPage] = useState(1)
   return (
     <Container>
       {loading && <h1>loading</h1>}
       {error && <h1>Error try refreshing</h1>}
-      <h1>{jobs.length}</h1>
+      {jobs.map(job => {
+        return <Job key={job.id} job={job} />
+      })}
     </Container>
   )
 }
