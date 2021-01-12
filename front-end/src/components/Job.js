20f135dcde60562d4f5bd9aa381d5ab73a12a40a
Yang Zhou <yzhou3991@gmail.com>
2021-01-12 17:15:26 -0500

    add more job card styling

--- front-end/src/components/Job.js
+++ front-end/src/components/Job.js
@@ -11,6 +11,11 @@ export default function Job({job}) {
               <Card.Title>
                 {job.title} - <span className="text-muted font-weight-light">{job.company}</span>
               </Card.Title>
+              <Card.Subtitle className="text-muted mb-2">
+              {new Date(job.created_at).toLocaleDateString()}
+              </Card.Subtitle>
+              <Badge variant="secondary" className="mr-2">{job.type}</Badge>
+              <Badge variant="secondary">{job.location}</Badge>
             </div>
         </div>
       </Card.Body>
