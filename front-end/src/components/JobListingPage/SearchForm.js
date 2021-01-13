5e5163c884eeaf9dfc12fb02be4c055bf4d13d1d
Yang Zhou <yzhou3991@gmail.com>
2021-01-12 22:45:00 -0500

    search by description and location

--- front-end/src/components/JobListingPage/SearchForm.js
+++ front-end/src/components/JobListingPage/SearchForm.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Form, Col } from 'react-bootstrap'
 
 export default function SearchForm({ params, onParamChange }) {
   return (
-    <div>
-      
-    </div>
+    <Form className="mb-4">
+    <Form.Row className="align-items-end">
+      <Form.Group as={Col}>
+        <Form.Label>Description</Form.Label>
+        <Form.Control onChange={onParamChange} value={params.description} name="description" type="text" />
+      </Form.Group>
+      <Form.Group as={Col}>
+        <Form.Label>Location</Form.Label>
+        <Form.Control onChange={onParamChange} value={params.location} name="location" type="text" />
+      </Form.Group>
+    </Form.Row>
+  </Form>
   )
 }
