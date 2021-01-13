ddc967cf34e9f00c129b7b11196f21eb341c4bbf
Yang Zhou <yzhou3991@gmail.com>
2021-01-12 21:59:59 -0500

    make pagination work

--- front-end/src/API/useFetchJobs.js
+++ front-end/src/API/useFetchJobs.js
@@ -5,7 +5,8 @@ import axios from 'axios'
 const ACTIONS = {
   MAKE_REQUEST: 'make-request',
   GET_DATA: 'get-data',
-  ERROR: 'error'
+  ERROR: 'error',
+  UPDATE_HAS_NEXT_PAGE: 'update-has-next-page'
 }
 
 const BASE_URL = 'https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json'
@@ -17,6 +18,8 @@ function reducer(state, action) {
       return { ...state, loading: false, jobs: action.payload.jobs }
     case ACTIONS.ERROR:
       return { ...state, loading: false, error: action.payload.error, jobs: [] }
+    case ACTIONS.UPDATE_HAS_NEXT_PAGE:
+      return { ...state, hasNextPage: action.payload.hasNextPage }
     default:
       return state
   }
@@ -26,10 +29,10 @@ export default function useFetchJobs(params,page) {
   const [state,dispatch] = useReducer(reducer,{jobs:[],loading:true})
 
   useEffect(() => {
-    const cancelToken = axios.CancelToken.source()
+    const cancelToken1 = axios.CancelToken.source()
     dispatch({type: ACTIONS.MAKE_REQUEST})
     axios.get(BASE_URL,{
-      cancelToken: cancelToken.token,
+      cancelToken: cancelToken1.token,
       params: {...params,markdown:true,page:page}
     }).then(res=>
       dispatch({type: ACTIONS.GET_DATA, payload:{jobs: res.data}})
@@ -37,8 +40,22 @@ export default function useFetchJobs(params,page) {
       if (axios.isCancel(e)){return};
       dispatch({type: ACTIONS.ERROR, payload:{error: e}})
     })
+
+    // check if it is the last page
+    const cancelToken2 = axios.CancelToken.source()
+    axios.get(BASE_URL, {
+      cancelToken: cancelToken2.token,
+      params: { markdown: true, page: page + 1, ...params }
+    }).then(res => {
+      dispatch({ type: ACTIONS.UPDATE_HAS_NEXT_PAGE, payload: { hasNextPage: res.data.length !== 0 } }) 
+    }).catch(e => {
+      if (axios.isCancel(e)) return
+      dispatch({ type: ACTIONS.ERROR, payload: { error: e } }) 
+    })
+
     return ()=>{
-      cancelToken.cancel()
+      cancelToken1.cancel()
+      cancelToken2.cancel()
     }
   }, [params,page])
 
--- front-end/src/pages/JobListingPage.js
+++ front-end/src/pages/JobListingPage.js
@@ -2,19 +2,24 @@ import React, { useState } from 'react';
 import useFetchJobs from '../API/useFetchJobs'
 import { Container } from 'react-bootstrap'
 import Job from '../components/JobListingPage/Job'
+import JobPagination from '../components/JobListingPage/JobPagination';
+
 
 export default function JobListingPage() {
-  const { jobs, loading, error } = useFetchJobs()
   const [params, setParams] = useState({})
   const [page, setPage] = useState(1)
+  const { jobs, loading, error, hasNextPage } = useFetchJobs(params, page)
+  
   return (
     <Container className="my-4">
       <h1 className = "mb-4">Tech Jobs</h1>
+      <JobPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
       {loading && <h1>loading</h1>}
       {error && <h1>Error try refreshing</h1>}
       {jobs.map(job => {
         return <Job key={job.id} job={job} />
       })}
+      <JobPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
     </Container>
   )
 }
