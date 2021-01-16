60d2fbb6a90b25f3cb475accd9b41b28cb61d32a
Yang Zhou <yzhou3991@gmail.com>
2021-01-15 21:46:12 -0500

    add zipcode bar

--- front-end/src/pages/JobSkillSurveyPage.js
+++ front-end/src/pages/JobSkillSurveyPage.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import SkillsToJobForm from '../components/SkillsToJobForm'
+import { useState, useEffect } from 'react'
+
 
 export default function JobListingsPage() {
+  const [lattitude,setLattitude] = useState(0)
+  const [longitude,setLongitude] = useState(0)
+  const [consent,setContent] = useState(false)
+  const [showForm,setShowForm] = useState(false)
+  const [zipcode,setZipcode] = useState('')
+
+  const getCurrentZipcode = (e)=>{
+
+  }
+
   return (
     <div>
-      <SkillsToJobForm />
+      <h4>To make your serach more accurate, please enter your zipcode</h4>
+      <input id = 'zipcode' placeholder='zipcode' ></input>
+      <button onClick ={getCurrentZipcode}>get location</button>
+      <button onClick ={(e)=>{setShowForm(true)}}>confirm</button>
+      {showForm && <SkillsToJobForm />}
     </div>
   )
 }
