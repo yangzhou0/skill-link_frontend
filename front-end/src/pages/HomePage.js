67324c1204034a5a964fca2a6bc851884e2bd0cc
dylansouthwick <dylansouthwick.ds@gmail.com>
2021-01-18 12:40:43 -0500

    Video_background

--- front-end/src/pages/HomePage.js
+++ front-end/src/pages/HomePage.js
@@ -6,7 +6,7 @@ import './css/homePage.css'
 export default function HomePage() {
   return (
     <div className="hero-container">
-      <video src="/videos/video-1.mp4" autoPlay loop muted/>
+      <video src="/videos/video-3.mp4" autoPlay loop muted/>
       <h1>SKILL - LINK</h1>
       <p>Your Future Starts Here </p>
       <div className="hero-btns">
